a93a4778d935620234f433587ae55ec1a0b606c1
jmjxperia <jmjxperia@users.noreply.github.com>
2020-10-28 19:41:37 +0530

    added ExtentReports

--- src/test/java/e2e/Listeners.java
+++ src/test/java/e2e/Listeners.java
@@ -1,15 +1,35 @@
 package e2e;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import resources.Base;
+import resources.ExtentReporterNG;
 
 import java.io.IOException;
 
 public class Listeners extends Base implements ITestListener {
+    ExtentTest test;
+    ExtentReports extent = ExtentReporterNG.getReportObject();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        test=extent.createTest(result.getMethod().getMethodName());
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.log(Status.PASS,"Test Passed");
+    }
+
     @Override
     public void onTestFailure(ITestResult result) {
+        test.fail(result.getThrowable());
         WebDriver driver=null;
         String testMethodName=result.getMethod().getMethodName();
         try {
@@ -24,5 +44,10 @@ public class Listeners extends Base implements ITestListener {
             e.printStackTrace();
         }
 
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
     }
 }
