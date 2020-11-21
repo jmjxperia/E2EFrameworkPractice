2913d1d265c0ad95c2536f75851dbb4135f0b2c0
jmjxperia <jmjxperia@users.noreply.github.com>
2020-11-21 15:17:39 +0530

    integrate cucumber with testng

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -1,12 +1,11 @@
 package cucumberOptions;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
 @CucumberOptions(
         features = "src/test/java/features",
         glue ="stepDefinitions")
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
